92619f8996db93e2fdd80cda2093db610a6fe256
NMosch <nmosch@gmail.com>
2020-06-28 22:15:33 -0700

    updated Schema

--- models/workout.js
+++ models/workout.js
@@ -1,7 +1,26 @@
 const {Schema, model} = require("mongoose");
 
 const workoutSchema = new Schema ({
-    
+    day: Date,
+    exercises: [{
+        type: {
+            type: String,
+            required: "Select type"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Please name this exercise"
+        },
+        duration: {
+            type: Number,
+            required: "How long was this exercise?"
+        },
+        weight: Number,
+        reps: Number,
+        sets: Number,
+        distance: Number,
+    }]
 })
 
 const Workout = model ("Workout", workoutSchema);
