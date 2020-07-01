25fda0d8ed142cc6c700b8629dae1289d8b92e95
NMosch <nmosch@gmail.com>
2020-06-30 22:46:38 -0700

    revised controller and post routes

--- controllers/workout-contoller.js
+++ controllers/workout-contoller.js
@@ -1,10 +1,54 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 const db = require("../models");
+
+router.get("/exercise", (req,res)=>{
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+})
+
+router.get("/stats", (req,res)=>{
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+})
 
 router.get("/api/workouts/range", async (req, res) => {
     try {
         const data = await db.Workout.find({});
+        res.json(data);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+})
+
+router.get("/api/workouts", async (req, res)=>{
+    try {
+        const data = await db.Workout.find({});
+
+        console.log(data);
+        res.json(data);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+})
+
+router.put("/api/workouts/:id", async (req, res)=>{
+    try {
+        const data = await db.Workout.findByIdAndUpdate(req.params.id,
+            {
+                $push: {exercises:req.body}
+            });
+            res.json(data);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+})
+
+router.post("/api/workouts", async (req,res)=>{
+    try {
+        const data = await db.Workout.create(req.body);
         res.json(data);
     } catch (error) {
         console.log(error);
--- models/workout.js
+++ models/workout.js
@@ -1,27 +1,37 @@
 const {Schema, model} = require("mongoose");
 
 const workoutSchema = new Schema ({
-    day: Date,
+    day: {
+        type:Date,
+        default:new Date().setDate(new Date().getDate())
+        },
     exercises: [{
         type: {
             type: String,
-            required: "Select type"
+            // required: "Select type"
         },
         name: {
             type: String,
             trim: true,
-            required: "Please name this exercise"
+            // required: "Please name this exercise"
         },
         duration: {
             type: Number,
-            required: "How long was this exercise?"
+            // required: "How long was this exercise?"
         },
         weight: Number,
         reps: Number,
         sets: Number,
-        distance: Number,
+        distance: Number
     }]
 })
+
+workoutSchema.methods.totalDuration = function () {
+    const durationTotal = this.exercises.reduce((accum, cv)=>{
+       return accum + cv.duration
+    }, 0);
+    return durationTotal;
+}
 
 const Workout = model ("Workout", workoutSchema);
 
